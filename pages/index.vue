ffe10d94196e64eb1eb42d73c99a640d0f38f539
hiroto940 <hiroto091488@gmail.com>
2020-09-24 15:39:24 +0900

    全て消した

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        sample-app
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <center><h1>Inovange</h1></center>
+
 </template>
 
 <script>
