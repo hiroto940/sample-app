1268d4a8c73c2d8fbc9367a2fd5a2fca186c3915
hiroto940 <hiroto091488@gmail.com>
2020-10-09 11:57:19 +0900

    修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,5 @@
 <template>
   <div>
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
-      <button
-        class="navbar-toggler position-absolute d-md-none collapsed"
-        type="button"
-        data-toggle="collapse"
-        data-target="#sidebarMenu"
-        aria-controls="sidebarMenu"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
-        </li>
-      </ul>
-    </nav>
-
     <div class="container-fluid">
       <div class="row">
         <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
@@ -34,7 +14,7 @@
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="file"></span>
-                  member
+                  members
                 </a>
               </li>
               <li class="nav-item">
@@ -56,26 +36,10 @@
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
           <div
             class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Inovange</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-              <div class="btn-group mr-2">
-                <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-              </div>
-              <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                <span data-feather="calendar"></span>
-                This week
-              </button>
-            </div>
+            <h1 class="h2">Dashboard</h1>
           </div>
-          What' Inovange
-          <br>Inovangeとは福井県出身の曲者が
-          <br>member
-          <br>works
-          <br>contact
-          <br>Inovange@sample.com
           <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-          </main>
+        </main>
       </div>
     </div>
   </div>
@@ -191,5 +155,4 @@ body {
   border-color: transparent;
   box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
 }
-
 </style>
